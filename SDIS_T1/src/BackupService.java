c66b7ffbe6317630dd2798ffb264db558fd202ba
Tiago Fernandes <ei11054@fe.up.pt>
2014-03-10 16:52:18 +0000

    first commit

---
+++ SDIS_T1/src/BackupService.java
@@ -0,0 +1,44 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BackupService {
+	
+	private InetAddress mcAddress;
+	private int mcPort;
+	private InetAddress mdbAddress;
+	private int mdbPort;
+	private InetAddress mdrAddress;
+	private int mdrPort;
+	private int diskSpace;
+	private Map<String, Integer> files;
+	
+	public static void main(String[] args) {
+		
+		if(args.length != 6) {
+			System.out.println("<MC_ADDRESS> <MC_PORT> <MDB_ADDRESS> <MDB_PORT> <MDR_ADDRESS> <MDR_PORT>");
+			return;
+		}
+		
+		try {
+			BackupService service=new BackupService(args);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public BackupService(String args[]) throws UnknownHostException {
+		
+		this.mcAddress =  InetAddress.getByName(args[0]);
+		this.mcPort = Integer.parseInt(args[1]);
+		this.mdbAddress = InetAddress.getByName(args[2]);
+		this.mdbPort = Integer.parseInt(args[3]);
+		this.mdrAddress = InetAddress.getByName(args[4]);
+		this.mdrPort = Integer.parseInt(args[5]);
+		
+		this.diskSpace=0;
+		this.files=new HashMap<String, Integer>();
+	}
+
+}
